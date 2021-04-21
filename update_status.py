b0a0b18473bae480d9eafc77adc2e82100da0639
MValencia111 <70298511+MValencia111@users.noreply.github.com>
2021-04-21 09:11:18 -0500

    added update_status.py

--- update_status.py
+++ update_status.py
@@ -1,1 +1,6 @@
-def update_status
+def update_status(conn):
+	if conn is not None: 
+		cur = conn.cursor()
+		cur.execute("UPDATE rooms JOIN reservation ON rooms.room_num = reservation.room_num SET rooms.status="Available" WHERE reservation.room_num = ?, (room_num)";
+
+
